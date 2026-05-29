7ed70a0b5f4ffc5849af6d770339be3cbf356b84
OsukaruMxtest <obz2k12@gmail.com>
2026-05-28 18:31:34 -0600

    updates

---
+++ eventos/SRD-MAYO/config.js
@@ -0,0 +1,9 @@
+const eventName = "SRD-MAYO";
+const eventPath = "/eventos/SRD-MAYO/";
+const overlays = [
+  "/eventos/SRD-MAYO/barras_SRD-MAYO.html",
+  "/eventos/SRD-MAYO/alertas_SRD-MAYO.html",
+  "/eventos/SRD-MAYO/final_SRD-MAYO.html",
+  "/eventos/SRD-MAYO/overlay_control.html"
+];
+const configPath = "/eventos/SRD-MAYO/event_config.json";
--- eventos/srd/config.js
+++
@@ -1,9 +0,0 @@
-const eventName = "srd";
-const eventPath = "/eventos/srd/";
-const overlays = [
-  "/eventos/srd/barras_srd.html",
-  "/eventos/srd/alertas_srd.html",
-  "/eventos/srd/final_srd.html",
-  "/eventos/srd/overlay_control.html"
-];
-const configPath = "/eventos/srd/event_config.json";
